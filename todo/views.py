e6e74a76a29a0dc4a63a2e0403be60b16b107251
sj <bcj0114@gmail.com>
2022-04-13 19:39:04 +0900

    2022-04-13 [todo] views.py create

--- todo/views.py
+++ todo/views.py
@@ -1,11 +1,22 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticated
 
+from todo.models import Todo
+from todo.permissions import IsOwner
 from todo.serializers import TodoSerializer
 
 
 class TodoListCreateAPIView(ListCreateAPIView):
     serializer_class = TodoSerializer
 
+    def get_queryset(self):
+        return Todo.objects.filter(owner=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
 
 class TodoDetailAPIView(RetrieveUpdateDestroyAPIView):
     serializer_class = TodoSerializer
+    queryset = Todo.objects.all()
+    permission_classes = [IsAuthenticated, IsOwner]     # login한 user이며, 게시물의 주인이어야만 함.
