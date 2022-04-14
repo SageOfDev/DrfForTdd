9b39dd2f58e8fda9bf77c2253e6a08629b45e60e
sj <bcj0114@gmail.com>
2022-04-14 11:36:14 +0900

    2022-04-14 [todo] admins.py create

--- DrfForTdd/settings.py
+++ DrfForTdd/settings.py
@@ -16,11 +16,11 @@ from pathlib import Path
 
 from django.core.exceptions import ImproperlyConfigured
 
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 secrets_file = BASE_DIR / "secrets.json"
-
 
 with open(secrets_file) as f:
     secrets = json.loads(f.read())
--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from todo.models import Todo
+
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ("owner", "name", "done", "date_created")
+    list_filter = ("done", "date_created")
+
+
+admin.site.register(Todo, TodoAdmin)
